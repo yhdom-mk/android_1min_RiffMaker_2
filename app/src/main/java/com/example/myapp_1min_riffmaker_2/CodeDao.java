c885ce644788a673fc5ec4a431905c318b58ab67
Ydom Mski <lcydome.msk12@gmail.com>
2022-08-06 23:55:31 +0900

    add DaoClass, DatabaseClass

--- app/src/main/java/com/example/myapp_1min_riffmaker_2/CodeDao.java
+++ app/src/main/java/com/example/myapp_1min_riffmaker_2/CodeDao.java
@@ -1,4 +1,26 @@
 package com.example.myapp_1min_riffmaker_2;
 
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface CodeDao {
+    @Query("SELECT * FROM codes")
+    List<Code> getAll();
+
+    @Query("SELECT * FROM codes WHERE id IN (:ids)")
+    List<Code> loadAllByIds(int[] ids);
+
+    @Insert
+    void insertAll(Code... code);
+    @Insert
+    void insert(Code id);
+    @Delete
+    void delete(Code code);
 }
--- app/src/main/java/com/example/myapp_1min_riffmaker_2/CodeDatabase.java
+++ app/src/main/java/com/example/myapp_1min_riffmaker_2/CodeDatabase.java
@@ -1,4 +1,9 @@
 package com.example.myapp_1min_riffmaker_2;
 
-abstract class CodeDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = { Code.class }, version = 1, exportSchema = false)
+public abstract class CodeDatabase extends RoomDatabase {
+    public abstract CodeDao codeDao();
 }
