0ecc164d09c36eee47ceeb0da2c5ceab554ecb87
Ydom Mski <lcydome.msk12@gmail.com>
2022-08-06 23:13:24 +0900

    add gradle roomtesting, add entity class

--- app/src/main/java/com/example/myapp_1min_riffmaker_2/Code.java
+++ app/src/main/java/com/example/myapp_1min_riffmaker_2/Code.java
@@ -1,4 +1,32 @@
 package com.example.myapp_1min_riffmaker_2;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+//import androidx.room.annotation.NonNull;
+@Entity(tableName = "codes")
 public class Code {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "code")
+    private String code;
+
+    public Code(String code){
+        this.code = code;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setCode(String code){
+        this.code = code;
+    }
+    public String getCode(){
+        return code;
+    }
 }
